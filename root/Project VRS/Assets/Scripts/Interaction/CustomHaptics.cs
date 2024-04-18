74a22472d013625db640ab0763216a9e687544d9
Wyatt Murray <wyattmurray1235@gmail.com>
2024-04-18 15:24:08 -0400

    some minor work on the custom haptics script

--- root/Project VRS/Assets/Scripts/Interaction/CustomHaptics.cs
+++ root/Project VRS/Assets/Scripts/Interaction/CustomHaptics.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+[RequireComponent(typeof(XRBaseInteractable))]
 public class CustomHaptics : MonoBehaviour
 {
     public bool SendHapticsToController;
     public List<XRBaseController> registeredControllers = new List<XRBaseController>();
+
+    public bool RecieveHover, RecieveSelect, RecieveActivate;
+    [Range(0f, 1f)]
+    public float HoverHapticIntensity, SelectHapticIntensity, ActivateHapticIntensity;
+
+    //we need to know waht controllers are interacting with this haptic
+
+    private void Awake()
+    {
+        registeredControllers.Clear();
+        XRBaseInteractable associatedInteractable = GetComponent<XRBaseInteractable>();
+       
+    }
+
+    void SetupInteractionEvents(XRBaseInteractable XRinteractable)
+    {
+        if(RecieveHover)
+        {
+            //XRinteractable.hoverEntered.AddListener(HoverHapticIntensity, )
+        }
+        if (RecieveSelect)
+        {
+            
+        }
+        if (RecieveActivate)
+        {
+
+        }
+    }
+
 
     public void RegisterInteractor()
     {
@@ -14,6 +45,11 @@ public class CustomHaptics : MonoBehaviour
     }
 
     public void DeRegisterInteractor()
+    {
+
+    }
+
+    private void ApplyHapticsToControllers(float intensity, float duration, bool loop)
     {
 
     }
