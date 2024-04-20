950111e6a0646f9537e009f95222dee7b7e2c09c
Wyatt Murray <wyattmurray1235@gmail.com>
2024-04-20 09:37:51 -0400

    BC_InteractionRegister is completed

--- root/Project VRS/Assets/Scripts/Ship Systems/Ship Core Systems/Avionics Module/BC_InteractionRegister.cs
+++ root/Project VRS/Assets/Scripts/Ship Systems/Ship Core Systems/Avionics Module/BC_InteractionRegister.cs
@@ -1,29 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+/// <summary>
+/// This class manages the registration of XRBaseInteractors with a game object.
+/// It provides functionality to register, deregister, and check for the currently registered interactor.
+/// </summary>
 public class BC_InteractionRegister : MonoBehaviour
 {
-
     //this is for registering the interactor, NOT the interactable
-
-    public XRBaseInteractor interactor;
     [SerializeField]
-    private XRBaseInteractable associatedInteractable;
-
-    protected void Awake()
+    protected XRBaseInteractor m_registeredInteractor;
+    public XRBaseInteractor RegisteredInteractor
     {
-        
+        get { return m_registeredInteractor; }
+    }
+    [SerializeField]
+    protected XRBaseInteractable m_associatedInteractable;
+
+    /// <summary>
+    /// Registers an XRBaseInteractor with the current object.
+    /// </summary>
+    /// <param name="args">The arguments associated with the interaction event.</param>
+    protected void RegisterInteractor(BaseInteractionEventArgs args)
+    {
+        m_registeredInteractor = args.interactorObject as XRBaseInteractor;
     }
 
-    protected void RegisterInteractor(BaseInteractionEventArgs arg)
+    /// <summary>
+    /// Deregisters the currently registered XRBaseInteractor.
+    /// </summary>
+    protected void DeRegisterInteractor(BaseInteractionEventArgs args)
     {
-
+        m_registeredInteractor = null;
     }
 
-    protected void DeRegisterInteractor(BaseInteractionEventArgs arg)
+    public bool HasRegistered()
     {
-
+        if(m_registeredInteractor != null)
+        {
+            return true;
+        }
+        return false;
     }
 }
