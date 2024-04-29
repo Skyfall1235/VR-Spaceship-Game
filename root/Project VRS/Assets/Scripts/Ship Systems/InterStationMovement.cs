80946ce7f985e0b422e612d9f59e59ef341cb8b5
Wyatt Murray <wyattmurray1235@gmail.com>
2024-04-29 03:25:44 -0400

    movewd the struct into the scriptable object for the chair movement.. made a bunch of comments so i can come back to this and work on it tomorrow. im done for the night

--- root/Project VRS/Assets/Scripts/Ship Systems/InterStationMovement.cs
+++ root/Project VRS/Assets/Scripts/Ship Systems/InterStationMovement.cs
@@ -3,19 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using static InterStationMovement;
 
+// oh this one is gonna be funny
 public class InterStationMovement : MonoBehaviour
 {
     //struct of the nodes and sub nodes, as well as a settings struct inside it
     //seat adsjustment data can just be a vector3 from the local origin
-    public List<RawGameobjectPositions> gameObjectPositions;
+    public List<SavedNodeData.RawGameobjectPositions> gameObjectPositions;
 
+    //this is the scriptable object associated for whatever shi pthis is. this is where we will store and access our baked positions
     [SerializeField]
     private SavedNodeData m_savedNodeData;
     public SavedNodeData SavedNodeData
     { get { return m_savedNodeData; } }
 
+    //this is used internally and externally to confirm if we can act on the data within
     public bool UseNodeData
     {
         get
@@ -28,12 +30,9 @@ public class InterStationMovement : MonoBehaviour
         }
     }
 
-    [Serializable]
-    public struct RawGameobjectPositions
-    { 
-        public GameObject Node;
-        public List<GameObject> subNode;
-    }
+    
+
+    //space for future actual stuff
 
 
 
@@ -48,10 +47,18 @@ public class InterStationMovement : MonoBehaviour
 [Serializable]
 public class SavedNodeData : ScriptableObject
 {
+    //the node storage
     [SerializeField]
     private List<NodeData> m_nodes;
     public List<NodeData> Nodes
     { get { return m_nodes; } }
+
+    //our current node that we are going to. probably need a bool to check if we are here, and if so to make this null?
+    //idk ill think of it in class probably
+    [SerializeField]
+    private NodeData m_currentNode;
+    public NodeData currentNode
+    { get { return m_currentNode; } }
 
     public void BakeNodeData(List<RawGameobjectPositions> rawData)
     {
@@ -60,15 +67,28 @@ public class SavedNodeData : ScriptableObject
     }
 
 
-    public class NodeData
+    public class NodeData//this is class because seat positions can change on any given frame so we trade memory for fewer instructions to model the behavior
     {
         public Vector3 NodePosition;
         public List<Vector3> SubNodePosition;
         public Vector3 CustomSeatAdjustment;
+        //unity event here? maybe somewhere else?? idk
+    }
+    //this is only needed in the So and like the class
+    [Serializable]
+    public struct RawGameobjectPositions
+    {
+        public GameObject Node;
+        public List<GameObject> subNode;
     }
 }
 
 
+//im doing this entire baking process because i want a reason to learn how to use the editor stuff.
+// yes i am still going to go into thins with a performance mindset.
+//i bet you will read this colin, i have no idea what im doing but the unity learn stuff is surpisingly good at showing me
+//anyway, all this stuff will get its own script in due time, this is a prototype script.
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 [CustomEditor(typeof(InterStationMovement))]
 public class LevelScriptEditor : Editor
