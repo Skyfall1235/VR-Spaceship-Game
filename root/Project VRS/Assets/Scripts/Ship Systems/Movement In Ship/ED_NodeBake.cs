7e3383949b7bc858000dd589d62a9535ec5631e5
Wyatt Murray <wyattmurray1235@gmail.com>
2024-04-29 15:04:23 -0400

    Update ED_NodeBake.cs

--- root/Project VRS/Assets/Scripts/Ship Systems/Movement In Ship/ED_NodeBake.cs
+++ root/Project VRS/Assets/Scripts/Ship Systems/Movement In Ship/ED_NodeBake.cs
@@ -3,10 +3,6 @@ using UnityEngine;
 
 //im doing this entire baking process because i want a reason to learn how to use the editor stuff.
 // yes i am still going to go into thins with a performance mindset.
-//i bet you will read this colin, i have no idea what im doing but the unity learn stuff is surpisingly good at showing me
-//anyway, all this stuff will get its own script in due time, this is a prototype script.
-//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
 [CustomEditor(typeof(InterStationMovement))]
 public class ED_NodeBake : Editor
 {
